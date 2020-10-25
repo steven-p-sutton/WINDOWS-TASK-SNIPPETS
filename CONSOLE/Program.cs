1d8c8e3feb7958af5691390b6af8083e2bae0a33
steven-p-sutton <steven.p.sutton@gmail.com>
2020-10-25 17:52:01 +0000

    Added CONSOLE & UWP projects

---
+++ CONSOLE/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CONSOLE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
